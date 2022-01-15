e36920871bdbc761ed062cff404bbc7c92c83dfc
seprov <skapden@gmail.com>
2022-01-15 15:59:56 -0600

    commented collatz.s

--- collatz.s
+++ collatz.s
@@ -4,18 +4,17 @@
 .global main		/* have to do this */
 
 main:
-  	mov r0, #100		/* put imm 100 in r0 */
-  	mov r1, #0
-again: 	add r1, r1, #1
-	ands ip, r0, #1
-	beq even
-	add r0, r0, r0, LSL #1
-	add r0, r0, #1
-	b again
-even:	mov r0, r0, asr #1
-	subs r7, r0, #1
-	bne again
-/* halt:	b halt */
-halt:	mov r0, r1
-	mov r7, #1
-	svc 0
+  	mov r0, #100		/* set n=100 in r0 */
+  	mov r1, #0		/* set count to 0 in r1 */
+again: 	add r1, r1, #1		/* increment count */
+	ands ip, r0, #1		/* set Z flag to parity */
+	beq even		/* if Z=0, or parity was even,  goto even */
+	add r0, r0, r0, LSL #1	/* n*=3 */ 
+	add r0, r0, #1		/* n+=1 */ 
+	b again			/* loop */
+even:	mov r0, r0, asr #1	/* n/=2 */
+	subs ip, r0, #1		/* set Z flag to n==1 */
+	bne again		/* if Z=1, or n!=1, loop */
+halt:	mov r0, r1		/* put count in r0 */
+	mov r7, #1		/* put 1 in r7 to terminate */
+	svc 0			/* system call */
